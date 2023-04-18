f49622c9ac286d4bb3c44f6c1a331e1501908ceb
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-04-18 11:07:14 +0545

    Baisakh 5

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,27 @@
 @extends('layouts.app')
+@section('content')
+    <h2 class="font-bold text-4xl text-blue-700">Dashboard</h2> 
+    <hr class="h-1 bg-blue-200">
+    
+    <div class="mt-4 grid grid-cols-3 gap-10">
+
+        <div class="px-4 py-8 rounded-lg bg-blue-600 text-white flex justify-between">
+            <p class="font-bold text-lg">Total Items</p>
+            <p class="font-bold text-5xl">600</p>
+        </div>
+
+
+        <div class="px-4 py-8 rounded-lg bg-red-600 text-white flex justify-between">
+            <p class="font-bold text-lg">Total Categories</p>
+            <p class="font-bold text-5xl">8</p>
+        </div>
+
+
+        <div class="px-4 py-8 rounded-lg bg-green-600 text-white flex justify-between">
+            <p class="font-bold text-lg">Pending Orders</p>
+            <p class="font-bold text-5xl">19</p>
+        </div>
+
+
+    </div>
+@endsection

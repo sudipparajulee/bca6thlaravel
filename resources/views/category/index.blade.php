44dfa671c0b63e6bf9f53132f778fe36001da778
Sudip Parajuli <sudip.parajulee@gmail.com>
2023-04-20 11:05:03 +0545

    Baisakh 7

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -3,6 +3,10 @@
     <h2 class="font-bold text-4xl text-blue-700">Categories</h2> 
     <hr class="h-1 bg-blue-200">
 
+    <div class="my-4 text-right px-10">
+        <a href="#" class="bg-amber-400 text-black px-4 py-2 rounded-lg shadow-md hover:shadow-amber-300">Add Category</a>
+    </div>
+
     <table id="mytable" class="display">
         <thead>
             <th>Order</th>
@@ -13,7 +17,10 @@
             <tr>
                 <td>1</td>
                 <td>Watch</td>
-                <td>Edit Delete</td>
+                <td>
+                    <a href="#" class="bg-blue-600 text-white px-2 py-1 rounded shadow hover:shadow-blue-400">Edit</a>
+                    <a href="#" class="bg-red-600 text-white px-2 py-1 rounded shadow hover:shadow-red-400">Delete</a>
+                </td>
             </tr>
         </tbody>
     </table>
